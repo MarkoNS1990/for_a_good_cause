a8f1be24422351af73402a44a63499c2ac58a305
Marko Zecevic <marczecevic@gmail.com>
2021-05-20 19:05:58 +0200

    Fix heroku issue

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -10,7 +10,7 @@ class DonationsController < ApplicationController
         @donation = current_user.donations.new(charity_id: params[:charity_id])
     
         if @donation.save
-          redirect_to , notice: 'You donated 1$ successfully.'
+          redirect_to charities_path , notice: 'You donated 1$ successfully.'
         else
           redirect_to charities_path, alert: 'You cannot donate.'
         end
