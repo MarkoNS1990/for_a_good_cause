f0219c31b96e76d51c53bb5d4cc48a714c6250e1
Marko Zecevic <marczecevic@gmail.com>
2021-05-21 10:09:38 +0200

    Add total donations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
             '<p class="donated">Thank you for donating to this charity</p>'.html_safe
 
           else
-            button_to('Donate!', charity_donations_path(charity_id: charity.id),class:'btn btn-block my-btn', method: :post, redirect_to: charities_path)
+            button_to('Donate $1', charity_donations_path(charity_id: charity.id),class:'btn btn-block my-btn', method: :post, redirect_to: charities_path)
           end
     end
 
