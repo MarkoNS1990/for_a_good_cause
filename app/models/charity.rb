e2ed6cb7dd85af87d4a69a1d0fba15369eef8e77
Marko Zecevic <marczecevic@gmail.com>
2021-05-22 14:50:37 +0200

    Fix rubocop

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -4,6 +4,4 @@ class Charity < ApplicationRecord
 
   belongs_to :user
   has_many :donations, dependent: :destroy
-
-  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :charities
-  has_many :donations ,dependent: :destroy
+  has_many :donations, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  scope :with_charities_count, -> {joins(:charities).select("users.*,COUNT(charities.id) AS charities_count").group("users.id")}
+  # rubocop:disable Layout/LineLength
+  scope :with_charities_count, lambda {
+                                 joins(:charities).select('users.*,COUNT(charities.id) AS charities_count').group('users.id')
+                               }
+  # rubocop:enable Layout/LineLength
 end
